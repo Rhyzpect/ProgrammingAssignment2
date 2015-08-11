6a742da6ed4f3ecd3287191c39468a0beef48ef3
Rhyzpect <rhyzpect@gmail.com>
2015-08-11 20:15:55 +0800

    Create two functions that cache matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## It takes time to calculate the inverse of a large matrix and 
+## of a large set of matrices.  This is time consuming and takes up a lot of 
+## memory.  For efficiency, it is highly recommended that the matrices are
+## cached especially if there are identical matrices in the set. 
 
-## Write a short comment describing this function
+##The following functions store matrices and cache inverse matrices.
+
+## The makeCacheMatrix function cache the inverse of matrices so that
+## efficient calculation is achieved. The function stores four functions, i.e.,
+## MatrixStore, StoredMatrix, InverseMatrix and StoredInverse.
 
 makeCacheMatrix <- function(x = matrix()) {
+                inv <- NULL
+                MatrixStore <- function(y){ #function stores inputted matrix
+                        x <<- y
+                        inv <<- NULL 
+                        }
+                StoredMatrix <- function() x
+                InverseMatrix <- function(inverse) inv <<- inverse
+                StoredInverse <- function() inv #function stores the inverse mat
+                list(MatrixStore = MatrixStore,
+                     StoredMatrix = StoredMatrix,
+                     StoredInverse=StoredInverse,
+                     InverseMatrix=InverseMatrix)
+                }
 
-}
+## cacheSolve function first checked for StoredInverse.  
+## If the function finds what it is looking for in the cached data, 
+## it returns the inverse matrix.  However if not, it calculates the inverse
+## of the matrix using solve().
+## Then it returns a matrix that is the inverse of 'x'
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+cacheSolve <- function(x,...) {
+        
+        inv <- x$StoredInverse()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$StoredMatrix()
+        inv <- solve(data,...)
+        x$InverseMatrix(inv)
+        inv
+        }
